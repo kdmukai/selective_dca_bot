ef523460990c979769878d23ac0d7db9d4727823
Keith Mukai <keith.mukai@essaytagger.com>
2019-04-21 16:22:03 -0500

    Update exchanges_manager.py

--- src/selective_dca_bot/exchanges/exchanges_manager.py
+++ src/selective_dca_bot/exchanges/exchanges_manager.py
@@ -13,11 +13,11 @@ class ExchangesManager():
                 ex[EXCHANGE__BINANCE] = BinanceExchange(exchange['key'], exchange['secret'], exchange['watchlist'])
 
             # Also update the exchange's all-time watchlist
-            if not AllTimeWatchlist.get_watchlist(exchange=EXCHANGE__BINANCE):
+            if not AllTimeWatchlist.get_watchlist(exchange=exchange['name']):
                 AllTimeWatchlist.create(
-                    exchange=EXCHANGE__BINANCE,
+                    exchange=exchange['name'],
                     watchlist=','.join(exchange['watchlist'])
                 )
-            AllTimeWatchlist.update_watchlist(watchlist=exchange['watchlist'], exchange=EXCHANGE__BINANCE)
+            AllTimeWatchlist.update_watchlist(watchlist=exchange['watchlist'], exchange=exchange['name'])
 
         return ex
